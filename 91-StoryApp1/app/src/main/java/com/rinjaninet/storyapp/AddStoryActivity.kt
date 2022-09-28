6dd47a12434eea847fe00a313401d77095d9805c
Herianto Tanoyo <heri2000@live.com>
2022-09-28 23:09:19 +0700

    StoryApp1: Experimet with Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK, and finish() method

--- 91-StoryApp1/app/src/main/java/com/rinjaninet/storyapp/AddStoryActivity.kt
+++ 91-StoryApp1/app/src/main/java/com/rinjaninet/storyapp/AddStoryActivity.kt
@@ -2,10 +2,19 @@ package com.rinjaninet.storyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rinjaninet.storyapp.databinding.ActivityAddStoryBinding
 
 class AddStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddStoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddStoryUpload.setOnClickListener {
+            finish()
+        }
     }
 }
--- 91-StoryApp1/app/src/main/java/com/rinjaninet/storyapp/RegisterActivity.kt
+++ 91-StoryApp1/app/src/main/java/com/rinjaninet/storyapp/RegisterActivity.kt
@@ -2,10 +2,19 @@ package com.rinjaninet.storyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rinjaninet.storyapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            finish()
+        }
     }
 }
