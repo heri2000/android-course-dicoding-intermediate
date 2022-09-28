7c3091d52c93914fd102ef9e84ea5add4b62cbd3
Herianto Tanoyo <heri2000@live.com>
2022-09-29 00:14:47 +0700

    StoryApp1: Add row item layout

--- 91-StoryApp1/app/src/main/java/com/rinjaninet/storyapp/MainActivity.kt
+++ 91-StoryApp1/app/src/main/java/com/rinjaninet/storyapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.rinjaninet.storyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.rinjaninet.storyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +16,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnMainAddNewStory.setOnClickListener {
-            val addStoryIntent = Intent(this, AddStoryActivity::class.java)
-            startActivity(addStoryIntent)
-        }
-
         val token: String? = intent.getStringExtra(EXTRA_TOKEN)
-        if (token == null) {
-            val loginIntent = Intent(this, LoginActivity::class.java)
-            loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(loginIntent)
-            finish()
+        if (token == null) navigateToLogin()
+    }
+
+    private fun navigateToLogin() {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(loginIntent)
+        finish()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                navigateToLogin()
+            }
         }
+        return true
     }
 
     companion object {
-        public const val EXTRA_TOKEN = "extra_token"
+        const val EXTRA_TOKEN = "extra_token"
     }
 }
