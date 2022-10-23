340ff1cf4e20e3ec70c6df6b40fb0ed2c1c4d990
Herianto Tanoyo <heri2000@live.com>
2022-10-24 01:34:26 +0700

    MyStudentData: Latihan melakukan sorting dalam database Room

--- 23-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/MainActivity.kt
+++ 23-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.mystudentdata.adapter.StudentAndUniversityAdapter
@@ -12,6 +14,7 @@ import com.dicoding.mystudentdata.adapter.StudentListAdapter
 import com.dicoding.mystudentdata.adapter.StudentWithCourseAdapter
 import com.dicoding.mystudentdata.adapter.UniversityAndStudentAdapter
 import com.dicoding.mystudentdata.databinding.ActivityMainBinding
+import com.dicoding.mystudentdata.helper.SortType
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,9 +52,12 @@ class MainActivity : AppCompatActivity() {
                 getUniversityAndStudent()
                 true
             }
-
             R.id.action_many_to_many -> {
                 getStudentWithCourse()
+                true
+            }
+            R.id.action_sort -> {
+                showSortingPopupMenu()
                 true
             }
 
@@ -93,6 +99,29 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "getStudentWithCourse: $it")
             adapter.submitList(it)
         })
+    }
+
+    private fun showSortingOptionMenu(isShow: Boolean) {
+        val view = findViewById<View>(R.id.action_sort) ?: return
+        view.visibility = if (isShow) View.VISIBLE else View.GONE
+    }
+
+    private fun showSortingPopupMenu() {
+        val view = findViewById<View>(R.id.action_sort) ?: return
+        PopupMenu(this, view).run {
+            menuInflater.inflate(R.menu.sorting_menu, menu)
+            setOnMenuItemClickListener {
+                mainViewModel.changeSortType(
+                    when (it.itemId) {
+                        R.id.action_ascending -> SortType.ASCENDING
+                        R.id.action_descending -> SortType.DESCENDING
+                        else -> SortType.RANDOM
+                    }
+                )
+                true
+            }
+            show()
+        }
     }
 
     companion object {
