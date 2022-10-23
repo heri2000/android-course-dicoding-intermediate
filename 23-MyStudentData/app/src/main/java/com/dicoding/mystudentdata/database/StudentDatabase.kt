a84a215115b777f692c12c990484e089c3c1808b
Herianto Tanoyo <heri2000@live.com>
2022-10-23 18:56:13 +0700

    MyStudentData: Latihan pre-populate database Room

--- 23-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/database/StudentDatabase.kt
+++ 23-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/database/StudentDatabase.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.dicoding.mystudentdata.helper.InitialDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(
     entities =
@@ -25,12 +29,27 @@ abstract class StudentDatabase : RoomDatabase() {
         private var INSTANCE: StudentDatabase? = null
 
         @JvmStatic
-        fun getDatabase(context: Context): StudentDatabase {
+        fun getDatabase(context: Context, applicationScope: CoroutineScope): StudentDatabase {
             if (INSTANCE == null) {
                 synchronized(StudentDatabase::class.java) {
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
                             StudentDatabase::class.java, "student_database")
                         .fallbackToDestructiveMigration()
+                        .createFromAsset("student_database.db")
+                        //.addCallback(object: Callback() {
+                        //    override fun onCreate(db: SupportSQLiteDatabase) {
+                        //        super.onCreate(db)
+                        //        INSTANCE?.let { database ->
+                        //            applicationScope.launch {
+                        //                val studentDao = database.studentDao()
+                        //                studentDao.insertStudent(InitialDataSource.getStudents())
+                        //                studentDao.insertUniversity(InitialDataSource.getUniversities())
+                        //                studentDao.insertCourse(InitialDataSource.getCourses())
+                        //                studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
+                        //            }
+                        //        }
+                        //    }
+                        //})
                         .build()
                 }
             }
