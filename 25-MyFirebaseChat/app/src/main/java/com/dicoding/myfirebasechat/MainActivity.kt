25f48bf018ee14bb8a64e59788504f59e7bedb16
Herianto Tanoyo <heri2000@live.com>
2022-11-04 11:34:19 +0700

    MyFirebaseChat: successful logout

--- 25-MyFirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ 25-MyFirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.dicoding.myfirebasechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+
+        if (firebaseUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,6 +48,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun signOut() {
-
+        auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
